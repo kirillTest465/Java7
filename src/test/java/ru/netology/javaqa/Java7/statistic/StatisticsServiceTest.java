f6380fb3ecf43d71f8fbfbc642d83f88383515a5
Neto test <ki.dmitriev.yandex@gmail.com>
2024-12-21 06:36:06 +0300

    test branch

--- src/test/java/ru/netology/javaqa/Java7/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/javaqa/Java7/statistic/StatisticsServiceTest.java
@@ -20,4 +20,19 @@ import org.junit.jupiter.api.Assertions;
 
             Assertions.assertEquals(expected, actual);
         }
+
+
+        @Test
+        void findEquality() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
+            long expected = 13;
+
+            long actual = service.findMax(incomesInBillions);
+
+            Assertions.assertEquals(expected, actual);
+
+        }
     }
+
